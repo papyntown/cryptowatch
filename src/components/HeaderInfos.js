65b3b4d2ab90c1b16ed020615ef828ab89df5fe4
papyntown <realistedem@gmail.com>
2023-03-21 12:41:52 +0100

    Crypto Watch

--- src/components/HeaderInfos.js
+++ src/components/HeaderInfos.js
@@ -17,7 +17,7 @@ const HeaderInfo = () => {
             <ul className="title">
                 <li>
                     <h1>
-                        <img src="./assets/logo.png" alt="logo" /> Watch Tower
+                        <img src="./assets/logo.png" alt="logo" /> Crypto Watch
                     </h1>
                 </li>
                 <li>
